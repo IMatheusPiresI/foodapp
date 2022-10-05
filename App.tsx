79fc1f50df0069ae620c1dc552e201f053ca17d5
Matheus Pires de Sousa <sousa@bne-empregos.com.br>
2022-10-05 10:36:00 -0300

    ADD Redux-Persist foodCart

--- App.tsx
+++ App.tsx
@@ -7,16 +7,19 @@ import {ThemeProvider} from 'styled-components';
 import AppRoutes from './src/routes';
 
 import {Provider} from 'react-redux';
-import {store} from './src/store';
+import {persistor, store} from './src/store';
 
 import theme from './src/styles/theme';
+import {PersistGate} from 'redux-persist/integration/react';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <AppRoutes />
-      </ThemeProvider>
+      <PersistGate persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <AppRoutes />
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   );
 };
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,23 @@
 import {combineReducers, createStore} from 'redux';
 
+import {persistStore, persistReducer} from 'redux-persist';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import {foodCartReducer} from './ducks/foodCart';
+
+const persistConfig = {
+  key: 'cartFood',
+  storage: AsyncStorage,
+};
 
 const rootReducers = combineReducers({
   foodCartReducer,
 });
 
-export const store = createStore(rootReducers);
+const persistedReducer = persistReducer(persistConfig, rootReducers);
+
+const store = createStore(persistedReducer);
+const persistor = persistStore(store);
+
+export {store, persistor};
